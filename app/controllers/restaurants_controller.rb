5ea1ad06fa6ec8490da231cee2336a9bd1b1d19e
remclay <remmy.lauren@gmail.com>
2018-04-10 17:17:27 -0600

    Add controller actions and views for Restaurants/show and Restaurants/index.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,18 @@
 class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find_by(id: params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
 end
