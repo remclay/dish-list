9c9f5ae7f284f349373fdc81d25776c5965e980c
remclay <remmy.lauren@gmail.com>
2018-04-10 16:23:23 -0600

    Add seed data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do
+  User.create(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length = 6, max_length = 12)
+  )
+end
+
+3.times do
+  Restaurant.create(
+    name: Faker::Food.spice,
+    cuisine: Faker::Address.country,
+    location: Faker::Address.city
+  )
+end
+
+@restaurant_count = Restaurant.all.count
+
+(@restaurant_count * 2).times do
+  Dish.create(
+    name: Faker::Food.dish,
+    restaurant_id: rand(1..@restaurant_count)
+  )
+end
