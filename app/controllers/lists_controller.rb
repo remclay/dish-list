0ac6404df3a97d0cf7270bd175bfb4d17179c2db
remclay <remmy.lauren@gmail.com>
2018-04-11 08:37:25 -0600

    Add pry gem, build new and create actions and before_action in ListsController, add lists/new view and conditionally login into lists/index.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,40 @@
+require 'pry'
+
 class ListsController < ApplicationController
+  before_action :require_login, only: [:show]
 
   def index
     @lists = List.all
   end
-  
+
   def show
     @list = List.find_by(id: params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.user_id = current_user.id
+    if @list.save
+      redirect_to list_path(@list), alert: "New list successfully created"
+    else
+      flash[:alert] = @list.errors.full_messages.first
+      redirect_to new_list_path
+    end
+  end
+
+  private
+  def require_login
+    unless user_signed_in?
+      flash[:error] = "You must be logged in to access that page"
+      redirect_to new_user_registration_path
+    end
+  end
+
+  def list_params
+    params.require(:list).permit(:title)
   end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,5 @@ class List < ApplicationRecord
   belongs_to :user
   has_many :list_items
   has_many :dishes, through: :list_items
+  validates_presence_of :title
 end
