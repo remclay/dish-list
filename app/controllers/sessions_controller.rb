bb2f631ca537b8d4a4b9fdfea800be997951dc3d
remclay <remmy.lauren@gmail.com>
2018-04-12 12:34:11 -0600

    Add SessionsController#destroy. Add alerts to #create.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,24 @@ class SessionsController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
-    @user = User.find_by(email: params[:email])
+    @user = User.find_by(email: params[:user][:email])
     if @user
       if @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
-        redirect_to root_path
+        redirect_to root_path, alert: "Welcome back"
       else
         flash[:notice] = "Invalid password"
-        redirect_to new_sesison_path
+        redirect_to login_path
       end
     else
-      redirect_to new_session_path
+      redirect_to login_path, alert: "Incorrect username"
     end
+  end
+
+  def destroy
+    session.delete :user_id
+    redirect_to root_path, alert: "You have successfully logged out"
   end
 end
