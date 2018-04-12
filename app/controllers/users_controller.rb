d725a23b56b7588fc29ca90531db9bcc89df6347
remclay <remmy.lauren@gmail.com>
2018-04-12 12:34:52 -0600

    Change redirect path in UsersController#create.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to root_path, alert: "Sign up successful" #your_list?
     else
-      redirect_to new_user_path #'signup'
+      render 'users/new' #redirect_to new_user_path #'signup'
     end
   end
 
