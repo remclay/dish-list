a0b8d385547e807af5d828cfd529e31c6ab30d94
remclay <remmy.lauren@gmail.com>
2018-04-12 12:31:46 -0600

    Add validations to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :email, :password, presence: true
+  validates :password, length: { in: 6..10 }
+  validates :email, uniqueness: true
 end
