7088c5ca0bc0a81b819211fa5eefcadaadca7685
remclay <remmy.lauren@gmail.com>
2018-04-11 21:52:12 -0600

    Change user model relationship with lists to 'has_one' list. Amend seeds.rb and routes.

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-  resource :users, only: [:show] do
-    resources :lists
-  end
+  # resource :users, only: [:show] do
+  #   resources :lists
+  # end
 
   root to: 'welcome#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-3.times do
-  User.create(
+@list_titles = ["Family friendly", "Fast food", "Food trucks", "Pop-ups", "First date", "cheap n easy", "Birthday dinner", "Cheap eats", "Fancy schmancy", "Fine dining", "Romantic", "Best of 2018", "Personal favourites", "The Classics!"]
+
+5.times do
+  @user = User.create(
     email: Faker::Internet.email,
     password: Faker::Internet.password(min_length = 6, max_length = 12),
     name: Faker::Name.name
   )
+  List.create(user_id: @user.id, title: @list_titles.sample)
 end
 
-3.times do
+10.times do
   Restaurant.create(
     name: Faker::Food.spice,
     cuisine: Faker::Address.country,
@@ -24,28 +27,17 @@ end
 
 @restaurant_count = Restaurant.all.count
 
-(@restaurant_count * 10).times do
+(@restaurant_count * 2).times do
   Dish.create(
     name: Faker::Food.dish,
     restaurant_id: rand(1..@restaurant_count)
   )
 end
 
-@user_count = User.all.count
-@occasions = ["Family friendly", "Fast food", "Food trucks", "Pop-ups", "First date", "cheap n easy", "Birthday dinner", "Cheap eats", "Fancy schmancy", "Fine dining", "Romantic", "Best of 2018", "Personal favourites", "The Classics!"]
-
-(@user_count * 3).times do
-  List.create(
-    user_id: rand(1..@user_count),
-    status: "current",
-    title: @occasions.sample
-  )
-end
-
 @dish_count = Dish.all.count
 @list_count = List.all.count
 
-(@list_count * 3).times do
+(@dish_count * 2).times do
   ListItem.create(
     list_id: rand(1..@list_count),
     dish_id: rand(1..@dish_count)
