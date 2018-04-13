ca5756e19f037d718b9f65f6ee5ca45c216d264f
remclay <remmy.lauren@gmail.com>
2018-04-13 15:50:47 -0600

    Refactor DishesHelper#included_in_list method. Add rendering of add_remove_link partial to lists/show view.

--- app/helpers/dishes_helper.rb
+++ app/helpers/dishes_helper.rb
@@ -1,7 +1,7 @@
 module DishesHelper
 
   def included_in_list(dish)
-    @list = User.find_by(id: session[:user_id]).list
-    @list.dishes.include?(dish)
+    @list = List.find_by(id: session[:user_id])
+    @list.dish_ids.include?(dish.id)
   end
 end
