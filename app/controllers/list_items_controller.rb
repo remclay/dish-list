72cc8d9aacbee02d96acf0adfacecdf3b7d5f50f
remclay <remmy.lauren@gmail.com>
2018-04-13 16:52:32 -0600

    Add conditional logic to ListItemsController#tasted. Refactor ListItemsController#destroy.

--- app/controllers/list_items_controller.rb
+++ app/controllers/list_items_controller.rb
@@ -17,17 +17,22 @@ class ListItemsController < ApplicationController
 
   def tasted
     item = ListItem.find_by(list_id: params[:user_id], dish_id: params[:dish_id])
-    item.status = true
-    if item.save
-      redirect_to user_dishes_path(current_user), alert: "Hope that dish was tasty!"
+    if item
+      item.status = true
+      if item.save
+        redirect_to user_dishes_path(current_user), alert: "Hope that dish was tasty!"
+      else
+        redirect_to user_dishes_path(current_user), alert: "Dish could not be updated"
+      end
     else
       redirect_to user_dishes_path(current_user), alert: "Dish could not be updated"
     end
   end
 
   def destroy
-    items = current_user.list.list_items
-    items.find_by(dish_id: params[:dish_id]).destroy
+    ListItem.find_by(list_id: current_user.id, dish_id: params[:dish_id]).destroy
+    # items = current_user.list.list_items
+    # items.find_by(dish_id: params[:dish_id]).destroy
     redirect_to list_path, alert: "Dish removed"
   end
 end
