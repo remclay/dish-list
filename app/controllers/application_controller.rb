4cd01955cccaf6efbddfe9add93bdc3500b9f626
remclay <remmy.lauren@gmail.com>
2018-04-17 14:30:09 -0600

    Refactor ApplicationController#current_user and #logged_in? to leverage that. Refactor ApplicationHelper#current_user (for now).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,11 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @user = User.find_by(id: session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
-    session[:user_id]
+    !!current_user
   end
 
   # def require_logged_in
