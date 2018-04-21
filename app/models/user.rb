43a8698d3dbb03f262d2a526ef02dc70f29898e2
remclay <remmy.lauren@gmail.com>
2018-04-21 16:16:15 -0600

    Amend layouts to remove links to 'edit user' path. Add password limit on SecureRandom.hex in User model for OAuth log ins.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   # Logged in via OAuth
   def self.find_or_create_by_omniauth(auth_hash)
     self.where(:email => auth_hash['info']['email']).first_or_create do |user|
-      user.password = SecureRandom.hex
+      user.password = SecureRandom.hex(10)
     end
   end
 end
