7135840097c39637fe8fdca6b5e60afa2853a7b3
remclay <remmy.lauren@gmail.com>
2018-04-24 14:55:52 -0600

    Add custom validators to Dish class to ensure unique dishes and unique restaurants created. Amend password length in seeds.rb.

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -4,9 +4,44 @@ class Dish < ApplicationRecord
   has_many :lists, through: :list_items
   validates_presence_of :name
   accepts_nested_attributes_for :restaurant, reject_if: :reject_restaurant
+  validate :unique_dish, :unique_restaurant
 
   def reject_restaurant(attributes)
     attributes[:name].blank? || attributes[:cuisine].blank? || attributes[:location].blank?
+  end
+
+  def unique_restaurant
+    if self.restaurant_id == nil && self.restaurant.name != ""
+      @existing_restaurants = Restaurant.where(name: self.restaurant.name)
+      if !@existing_restaurants.empty?
+        if @existing_restaurants.any? do |restaurant|
+          restaurant.cuisine == self.restaurant.cuisine && restaurant.location == self.restaurant.location
+        end
+        errors.add(:restaurant_id, "Restaurant already exists")
+        # && @existing_restaurants.cuisine == self.restaurant.cuisine && @existing_restaurant.location == self.restaurant.location
+        # errors.add(:restaurant_id, "Restaurant already exists")
+        end
+      end
+    end
+  end
+
+  def unique_dish
+    @existing_dishes = Dish.where(name: self.name)
+    if !@existing_dishes.empty?
+      if @existing_dishes.any? do |dish|
+         dish.restaurant == self.restaurant
+         #exact dish exists, with selected restaurant
+         end
+         errors.add(:restaurant_id, "That dish already exists at the restaurant you selected. Please add the existing dish to your Dish-List")
+         #redirect_to restaurant // existing_dish
+      elsif
+        #exact dish exists, newly created restaurant already exists
+        @existing_dishes.any? do |dish|
+          dish.restaurant.name == self.restaurant.name && dish.restaurant.cuisine == self.restaurant.cuisine && dish.restaurant.location == self.restaurant.location
+        end
+        errors.add(:restaurant_id, "That dish and restaurant already exist. Please add the existing dish to your Dish-List")
+      end
+    end
   end
 
   def increase_popularity
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 5.times do
   @user = User.create(
     email: Faker::Internet.email,
-    password: Faker::Internet.password(min_length = 6, max_length = 12),
+    password: Faker::Internet.password(min_length = 6, max_length = 20),
     # name: Faker::Name.name
   )
   List.create(user_id: @user.id)
