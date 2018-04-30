74c3d5b585e307b518da62e94b960e12e4388696
remclay <remmy.lauren@gmail.com>
2018-04-30 10:44:42 -0600

    Add validate presence of name attribute to User class.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :name, presence: true
   validates :email, presence: true
   validates :password, length: { in: 6..20 }
   validates :email, uniqueness: true
