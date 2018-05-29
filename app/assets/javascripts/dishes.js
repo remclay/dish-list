a8ff4f0a39fdc5481531c986ebb79b8f71cb6b3a
remclay <remmy.lauren@gmail.com>
2018-05-29 17:22:17 +1000

    Amend dish show page next button functionality to retreive data from response which includes top level object. Begin refactoring new dish form submission to format html via prototype method.

--- app/assets/javascripts/dishes.js
+++ app/assets/javascripts/dishes.js
@@ -1,18 +1,19 @@
-// Show page, next button
 $(function () {
+  // Next dish button on dish show page
   $(".js-next").on("click", function(e) {
     e.preventDefault();
     var nextId = parseInt($(".js-next").attr("data-id")) + 1;
 
+    // fix this to ask for json not hard code json extension
     $.get("/dishes/" + nextId + ".json", function(data) {
       const dish = data
-
-      $(".dishName").text(dish["name"]);
-      $(".restaurantName").text(dish["restaurant"]["name"]);
-      $(".restaurantCuisine").text(dish["restaurant"]["cuisine"]);
-      $(".restaurantLocation").text(dish["restaurant"]["location"]);
+      // refactor into prototype method
+      $(".dishName").text(dish.data.attributes.name);
+      $(".restaurantName").text(dish.included[0].attributes.name);
+      $(".restaurantCuisine").text(dish.included[0].attributes.cuisine);
+      $(".restaurantLocation").text(dish.included[0].attributes.location);
       // // re-set the id to current id on 'next' link
-      $(".js-next").attr("data-id", data["id"]);
+      $(".js-next").attr("data-id", dish.data["id"]);
     });
   });
 
@@ -27,12 +28,14 @@ $(function () {
       data: params,
       dataType: "json",
       method: "POST"
-    });
+    })
     .done(function(json) {
       // append new dish to dom
-      const html = `<li><strong><a href="/dishes/${json.data.id}">${json.data.attributes.name}</a></strong> | Added to 1 Dish-List</li><br><br>`
-      $("#restaurant_show").append(html);
-    });
+      // const html = `<li><strong><a href="/dishes/${json.data.id}">${json.data.attributes.name}</a></strong> | Added to 1 Dish-List</li><br><br>`
+      const dish = new Dish({name: json.data.attributes.name, id: json.data.id, restaurant_id: json.data.attributes['restaurant-id'], popularity: 0})
+      dishHtml = dish.formatRestaurantDish()
+      $("#restaurant_show").append(dishHtml);
+    })
     .fail(function(response) {
       // if ajax post request doesn't work
       // TO DO
