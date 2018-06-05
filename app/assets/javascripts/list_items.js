918f99c3b194ecbab483b741ce3c8a66502cbf28
remclay <remmy.lauren@gmail.com>
2018-06-05 12:13:50 +1000

    Amend ListItem function to incorporate new attributes, build out .done callback, including ListItem.prototype.formatLI.

--- app/assets/javascripts/list_items.js
+++ app/assets/javascripts/list_items.js
@@ -1,8 +1,9 @@
 function ListItem(attributes) {
   this.id = attributes.id
-  this.list_id = attributes.list_id
   this.dish_id = attributes.dish_id
-  this.tasted = attributes.tasted
+  this.dish_name = attributes.dish_name
+  this.restaurant_name = attributes.restaurant_name
+  this.restaurant_location = attributes.restaurant_location
 }
 
 ListItem.remove = function(response) {
@@ -13,6 +14,22 @@ ListItem.fail = function(error) {
   // TO DO
   console.log("Broken", error)
 }
+
+ListItem.prototype.formatLI = function() {
+  html = ''
+  html += `<div class="inline">`
+  html += `<a href="/dishes/${this.dish_id}">${this.dish_name}</a> | ${this.restaurant_name} | ${this.restaurant_location}`
+  html += `</div><br>`
+  return html
+}
+
+//   <form id="remove_list_item" class="button_to" method="post" action="/list_items/8">
+//     <input type="hidden" name="_method" value="delete">
+//     <input type="submit" value="remove">
+//     <input type="hidden" name="authenticity_token" value="wVUuGhOYXQbIDIXHaUqCzqqw6ieLqOp5pUedftbED7XLgVmQVwRf9dvwwNsNWg0xXNNMEv+coIhpgNyE4OEjRw==">
+//     <input type="hidden" name="dish_id" value="1">
+//   </form>
+// </div>
 
 // On document ready or turbolinks load
 $( document ).on('ready turbolinks:load', function() {
@@ -35,6 +52,7 @@ $( document ).on('ready turbolinks:load', function() {
 // })
 
   // retrieve users list items
+  // NEED TO PROPERLY RETREIVE THIS
   const list_id = window.location.pathname.slice(-1)
   $.ajax({
     url: "/list_items",
@@ -44,12 +62,11 @@ $( document ).on('ready turbolinks:load', function() {
     }
   })
   .done(function(response) {
-    // debugger
-    jQuery.each(response, function() {
-    const list_item = new ListItem({id: json.data.attributes.name, list_id: json.data.id, dish_id: json.data.attributes['restaurant-id'], tasted: 0})
-    list_item.formatLI();
-  })
-
+    response.data.forEach(function(li) {
+      const list_item = new ListItem({id: li.id, dish_id: li.attributes["dish-id"], dish_name: li.attributes["dish-name"], restaurant_name: li.attributes["restaurant-name"], restaurant_location: li.attributes["restaurant-location"]})
+      list_item_HTML = list_item.formatLI();
+      $("#list_items").append(list_item_HTML)
+    })
   })
   .fail(function(error) {
     console.log(error)
